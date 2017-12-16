0b25d2a5f109cc2325a21131a2c532578fdaa6d5
Kara Mui <kara.mui@dartmouth.edu>
2017-12-15 21:14:30 -0600

    streamlined code, partial functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
-// make user lose when they dont answer in time
-// remove continue button -> restart game
-// fix timer start between questions
+// run questions sequentially
 
 $(document).ready(function() {
 	// hide elements
 	$("#controls").hide();
 	$(".questionbox").hide();
 	$(".answerbox").hide();
+	$("#restart").hide();
 
 	// declaring audio variables
 	var intromusic = document.getElementById("intromusic");
@@ -19,6 +18,31 @@ $(document).ready(function() {
 	var incorrect = 0;
 	var remaining = 10;
 	var result = "";
+	var timer = "";
+	var index = 0;
+
+	// trivia questions (array of objects)
+	var questionSelect = [
+		{qheader: "Question One",
+		qtext: "First question text.",
+		answer1: "Answer one.",
+		answer1value: "incorrect",
+		answer2: "Answer two.",
+		answer2value: "correct",
+		answer3: "Answer three.",
+		answer3value: "incorrect",
+		atext: "BLAH BLAH BLAH."},
+
+		{qheader: "Question Two",
+		qtext: "Second question text.",
+		answer1: "Answer one.",
+		answer1value: "correct",
+		answer2: "Answer two.",
+		answer2value: "incorrect",
+		answer3: "Answer three.",
+		answer3value: "incorrect",
+		atext: "BLAH."}
+	];
 
 	// play intro music on startup
 	intromusic.play();
@@ -35,56 +59,22 @@ $(document).ready(function() {
 		$(".fade1").fadeOut(1500);
 
 		// fade in controls
-		$(".fade2").delay(1500).fadeIn(1500);
+		$("#controls").delay(1500).fadeIn(1500);
 		audioControls(gamemusic);
 
 		// start game music
 		$("#gamemusic").prop("volume", 0);
 		setTimeout("gamemusic.play();", 3000);
 		$("#gamemusic").animate({volume: 0.5}, 3500);
+
+		//executeAsynchronously([question(0), question(1)]);	
 		
-		// run timer
-		var timer = setInterval(countDown, 1000);
-		console.log(remaining);
-
-		if (timer == 0) {
-			alert("hi");
-			incorrect++;
-			result = "Time's Up!"
-			incorrectsound.play();
-			clearInterval(timer);
-			updateScreen();
-		}
-
-		// user selects an answer
-		$(".answer").on("click", function() {
-			var userAnswer = $(this).val();
-			$("#question1").fadeOut(1000);
-			$(".answerbox").delay(1000).fadeIn(1000);
-
-			if (userAnswer == "correct" && remaining > 0) {
-				correct++;
-				result = "Correct!"
-				correctsound.play();
-				nextQuestion();
-			} else if (userAnswer == "incorrect" && remaining > 0) {
-				incorrect++;
-				result = "Incorrect!"
-				incorrectsound.play();
-				nextQuestion();
-			} else {
-				alert("Oops! Looks like you misclicked! Please try again.");
-			}
-		}); 
-
-		// 
-		function nextQuestion() {
-			clearInterval(timer);
-			updateScreen();
-			$(".fade3").delay(5000).fadeOut(1500);
-			remaining = 30;
-			//timer = setInterval(countDown, 1000);
-		}
+		//var indexTimer = setInterval("index = index + 1; console.log(index);", 17499);
+		//var runGame = setInterval(question(index), 17500);
+		
+		question(0);
+		//setTimeout(question(0), 1000);
+		//setTimeout(question(1), 10000);
 	});
 
 	// audio controls
@@ -116,15 +106,7 @@ $(document).ready(function() {
 		});
 	}
 
-	// update counters
-	function updateScreen() {
-		$("#correct").text(correct);
-		$("#incorrect").text(incorrect);
-		$("#remaining").text(remaining);
-		$("#result").text(result);
-	}
-
-	// timer
+	// timer and result of failing to select an answer
 	function countDown() {
 		if (remaining > 0) {
 			remaining--;
@@ -132,6 +114,87 @@ $(document).ready(function() {
 		} else {
 			remaining = 0;
 		}
+
+		if (remaining == 0) {
+			incorrect++;
+			result = "Time's Up!"
+			incorrectsound.play();
+			$(".questionbox").fadeOut(1000);
+			$("#atext").text(questionSelect[index].atext);
+			$(".answerbox").delay(1000).fadeIn(1000);
+			clearInterval(timer);
+			updateScreen();
+			setTimeout(nextQuestion, 5000);
+		}
+	}
+
+	// REFERENCE: https://stackoverflow.com/questions/5187968/how-should-i-call-3-functions-in-order-to-execute-them-one-after-the-other
+	function executeAsynchronously(functions) {
+  		for (var i = 0; i < functions.length; i++) {
+    		setTimeout(functions[i], 17500);
+  		}
+	}
+
+	// prepare to load next question
+	function nextQuestion() {
+		$(".answerbox").fadeOut(1500);
+		remaining = 10;
+		index++;
+	}
+
+	// question logic
+	function question(X) {
+		timer = setInterval(countDown, 1000);
+		$(".questionbox").delay(1500).fadeIn(1500);
+
+		questionText(X);
+
+		// user selects an answer
+		$(".answer").one("click", function() {
+			var userAnswer = $(this).val();
+			$(".questionbox").fadeOut(1000);
+			$(".answerbox").delay(1000).fadeIn(1000);
+
+			if (userAnswer == "correct") {
+				correct++;
+				result = "Correct!"
+				correctsound.play();
+				$("#atext").text(questionSelect[X].atext);
+				clearInterval(timer);
+				updateScreen();
+				setTimeout(nextQuestion, 5000);
+			} else if (userAnswer == "incorrect") {
+				incorrect++;
+				result = "Incorrect!"
+				incorrectsound.play();
+				$("#atext").text(questionSelect[X].atext);
+				clearInterval(timer);
+				updateScreen();
+				setTimeout(nextQuestion, 5000);
+			} else {
+				alert("Oops! Looks like you misclicked! Please try again.");
+			}
+		}); 
+	}
+
+	// push questions and answers to page
+	function questionText(X) {
+		$("#qheader").text(questionSelect[X].qheader);
+		$("#qtext").text(questionSelect[X].qtext);
+		$("#answer1").text(questionSelect[X].answer1);
+		$("#answer1").removeAttr("value").val(questionSelect[X].answer1value);
+		$("#answer2").text(questionSelect[X].answer2);
+		$("#answer2").removeAttr("value").val(questionSelect[X].answer2value);
+		$("#answer3").text(questionSelect[X].answer3);
+		$("#answer3").removeAttr("value").val(questionSelect[X].answer3value);
+	}
+
+	// update counters
+	function updateScreen() {
+		$("#correct").text(correct);
+		$("#incorrect").text(incorrect);
+		$("#remaining").text(remaining);
+		$("#result").text(result);
 	}
 
 }); 
